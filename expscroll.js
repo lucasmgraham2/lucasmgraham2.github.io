1e9837e045188ec695312d2ab24ad4703e2048c4
lucasmgraham2 <lmg00019@mix.wvu.edu>
2024-06-27 00:35:13 -0400

    start of structuring exp section

--- expscroll.js
+++ expscroll.js
@@ -1,25 +1,15 @@
-var items = document.querySelectorAll("pp");
+$(window).scroll(function(){				 
+	$('.container p').each(function(){
+    	var scrollTop     = $(window).scrollTop(),
+        	elementOffset = $(this).offset().top,
+       		distance      = (elementOffset - scrollTop),
+			    windowHeight  = $(window).height(),
+			    breakPoint    = windowHeight*0.9;
 
-function isItemInView(item){
-  var rect = item.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isItemInView(items[i])) {
-        items[i].classList.add("show");
-      }
-    }
-  }
-
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-
+			if(distance > breakPoint) {
+				$(this).addClass("more-padding");	
+			}  if(distance < breakPoint) {
+				$(this).removeClass("more-padding");	
+			}
+	});
+});
