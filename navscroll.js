62ec7b5c015ab13d2180de965b3c5dca9e76675b
lucasmgraham2 <lmg00019@mix.wvu.edu>
2024-07-02 00:05:20 -0400

    Add exp section bullet points. Simplify about. Create activities.

--- navscroll.js
+++ navscroll.js
@@ -4,18 +4,28 @@ function scrollFunction() {
     const nav = document.querySelector('.navbar_container');
     const space = document.querySelector('.spacing');
     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        //nav container smaller and adds background color
         nav.style.margin="0px";
         nav.style.backgroundColor="#282828";
         nav.style.maxHeight="70px";
         nav.style.width="95.99%";
+
+        
         name.style.fontSize="1.5em";
+
+        //spacing between name and links is smaller
         space.style.paddingRight="10px";
     }
     else {
+
         nav.style.margin="2%";
         nav.style.backgroundColor="transparent";
-        name.style.fontSize="40px";
         nav.style.maxHeight="100px";
-        space.style.paddingRight="200px";
+
+
+        name.style.fontSize="40px";
+       
+        //spacing between name and links is larger
+        space.style.paddingRight="100px";
     }
 }
