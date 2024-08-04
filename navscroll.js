4608af56ee5853dc8a201f57aa42cb165c7ac545
lucasmgraham2 <lmg00019@mix.wvu.edu>
2024-08-03 20:31:49 -0400

    Added basic responsiveness. Took out bootstrap to use media queries.

--- navscroll.js
+++ navscroll.js
@@ -3,18 +3,22 @@ function scrollFunction() {
     const name = document.querySelector('.navbar_name');
     const nav = document.querySelector('.navbar_container');
     const space = document.querySelector('.spacing');
+    const text_items = document.querySelector('.nav_link');
     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         //nav container smaller and adds background color
         nav.style.margin="0px";
         nav.style.backgroundColor="#282828";
         nav.style.maxHeight="59px";
         nav.style.width="95.99%";
-
-        
         name.style.fontSize="1.5em";
 
         //spacing between name and links is smaller
         space.style.paddingRight="10px";
+        if(window.innerWidth < 1175) {
+            name.style.fontSize="20px";
+            text_items.style.fontSize="18px";
+        }
+        
     }
     else {
 
@@ -26,6 +30,13 @@ function scrollFunction() {
         name.style.fontSize="40px";
        
         //spacing between name and links is larger
-        space.style.paddingRight="100px";
+        space.style.paddingRight="80px";
+
+        if(window.innerWidth < 1175) {
+            name.style.fontSize="20px";
+            text_items.style.fontSize="16px";
+            space.style.paddingRight="20px";
     }
 }
+}
+
