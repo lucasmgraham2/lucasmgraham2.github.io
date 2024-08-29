94116206803398579ee8698224f527b29936f140
lucasmgraham2 <lmg00019@mix.wvu.edu>
2024-08-28 22:14:13 -0400

    Edit title, icon, and nav dropdown

--- navresponsive.js
+++ navresponsive.js
@@ -10,13 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
             dropdown.style.display = 'none';
         }
     });
-    window.addEventListener('resize', function() {
-        if (window.innerWidth > 900) {
-            dropdown.style.display = 'flex'; // Ensure dropdown is visible on larger screens
-        } else if (dropdown.style.display !== 'flex') {
-            dropdown.style.display = 'none'; // Maintain state for small screens
-        }
-    });
 
     // Initial check when the page loads
     if (window.innerWidth > 900) {
--- navscroll.js
+++ navscroll.js
@@ -1,5 +1,3 @@
-
-
 window.onscroll = function() {scrollFunction()}
 function scrollFunction() {
     if(window.innerWidth > 900) {
